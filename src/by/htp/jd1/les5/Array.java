fa5cfb98383860ec9f572aa832fdb6b47885dac7
UserJavaUser <57154584+UserJavaUser@users.noreply.github.com>
2019-11-11 19:45:32 +0300

    Update and rename Arrays.java to Array.java

---
+++ src/by/htp/jd1/les5/Array.java
@@ -0,0 +1,24 @@
+package by.htp.jd1.les5;
+
+public class Array {
+
+	public static void main(String[] args) {
+		Exercise.exercise1();
+		Exercise.exercise2();
+		Exercise.exercise3();
+		Exercise.exercise4();
+		Exercise.exercise5();
+		Exercise.exercise6();
+		Exercise.exercise7();
+		Exercise.exercise9();
+		Exercise.exercise10();
+		Exercise.exercise12();
+		Exercise.exercise14();
+		Exercise.exercise15();
+		Exercise.exercise17();
+		Exercise.exercise19();
+		Exercise.exercise20();
+		
+	}
+
+}
